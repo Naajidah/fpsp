ecfbe9006f3c2ee5af7fbaea3a289954c3114fc1
Naajidah <vnaajidah@gmail.com>
2017-09-05 22:31:24 -0700

    Add files via upload

---
+++ fp scripts.js
@@ -0,0 +1,40 @@
+
+<script>
+  $( function() {
+    $( "#accordion" ).accordion({
+      active: false, 
+      collapsible: true
+    });
+  } );
+
+
+  $( function() {
+    $( "#accordion2" ).accordion({
+      active: false, 
+      collapsible: true
+    });
+  } );
+
+
+  $( function() {
+    $( "#accordion3" ).accordion({
+      active: false, 
+      collapsible: true
+    });
+  } );
+
+
+  $( function() {
+    $( "#accordion4" ).accordion({
+      active: false, 
+      collapsible: true
+    });
+  } );
+
+  $( function() {
+    $( "#accordion5" ).accordion({
+      active: false, 
+      collapsible: true
+    });
+  } );
+  </script>
